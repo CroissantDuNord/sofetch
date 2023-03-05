98db5dbc344f80bf4498ced7a87eb156427dbff1
JohnVictoryz <john.g.zioupos@gmail.com>
2023-03-05 16:51:35 +0200

    Done with linux installer

--- setup-linux.py
+++ setup-linux.py
@@ -6,10 +6,29 @@ print("Hello and Welcome to the installer for sofetch")
 print("Have you downloaded the repo locally [y/n]")
 input = input()
 if input == "y":
-     print("Provide full path to the root of the repository:")
-     inputrepo = input()
-     os.system("sudo mv ", inputrepo, "sofetch/* /usr/bin/")
-     print("done reload your terminal")
+    print("Provide full path to the root of the repository (with the slash):")
+    inputrepo = input()
+    if "Manjaro" in dist.name():
+        os.system("sudo pacman -S python3 python-pip")
+    elif "Fedora" in dist.name():
+        os.system("sudo dnf install python3 python3-pip")
+    elif "Ubuntu" in dist.name():
+        os.system("sudo apt install python3 python-pip")
+    elif "Debian" in dist.name():
+        os.system("sudo apt install python3 python-pip")
+    elif "Gentoo" in dist.name():
+        os.system("sudo emerge --ask dev-lang/python:3.10")
+        os.system("sudo emerge --ask dev-python/pip")
+    elif "Arch" in dist.name():
+        os.system("sudo pacman -S python3 python-pip")
+    elif "Mint" in dist.name():
+        os.system("sudo apt install python3 python-pip")
+    elif "Lite" in dist.name():
+        os.system("sudo apt install python3 python-pip")
+    elif "Garuda" in dist.name():
+        os.system("sudo pacman -S python3 python-pip")
+    os.system("pip install --use-pep517 -r ", inputrepo, "requirements.txt && sudo mv ", inputrepo, "sofetch/* /usr/bin/ && sudo mv /usr/bin/sofetch.py /usr/bin/sofetch && sudo chmod +x /usr/bin/sofetch")
+    print("done..")
 elif input == "n":
      print("Ok dont worry we will do some things to get it ready")
      if "Manjaro" in dist.name():
@@ -17,17 +36,20 @@ elif input == "n":
     elif "Fedora" in dist.name():
         os.system("sudo dnf install git python3 python3-pip")
     elif "Ubuntu" in dist.name():
-        os.system("sudo apt install python3 python-pip")
+        os.system("sudo apt install git python3 python-pip")
     elif "Debian" in dist.name():
-        os.system("sudo apt install python3 python-pip")
+        os.system("sudo apt install git python3 python-pip")
     elif "Gentoo" in dist.name():
-        os.system
+        os.system("sudo emerge --ask dev-lang/python:3.10")
+        os.system("sudo emerge --ask dev-python/pip")
+        os.system("sudo emerge --ask dev-vcs/git")
     elif "Arch" in dist.name():
-        print(arch)
+        os.system("sudo pacman -S git python3 python-pip")
     elif "Mint" in dist.name():
-        print(mint)
+        os.system("sudo apt install git python3 python-pip")
     elif "Lite" in dist.name():
-        print(lite)
+        os.system("sudo apt install git python3 python-pip")
     elif "Garuda" in dist.name():
-        print(garuda)
-#NOT READY 
+        os.system("sudo pacman -S git python3 python-pip")
+    os.system("cd /tmp && git clone --recursive https://github.com/Soviet-Linux/sofetch && pip install --use-pep517 -r requirements.txt && sudo mv /tmp/sofetch/sofetch/* /usr/bin/ && sudo mv /usr/bin/sofetch.py /usr/bin/sofetch && sudo chmod +x /usr/bin/sofetch && rm -rf /tmp/sofetch/ && cd ~")
+    print("done..")
