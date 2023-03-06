cb5faac6c6421bf2e185de3bb779a6ca90b489be
JohnVictoryz <john.g.zioupos@gmail.com>
2023-03-06 22:28:19 +0200

    Modifed to make it better for soviet-linux pkg manager

--- setup-linux.py
+++ setup-linux.py
@@ -2,6 +2,12 @@ import distro as dist
 import sys
 import os
 
+
+if sys.argv[1] == "-soviet":
+     os.system("cd /tmp && git clone --recursive https://github.com/Soviet-Linux/sofetch && pip install --use-pep517 -r requirements.txt && sudo mv /tmp/sofetch/sofetch/* /usr/bin/ && sudo mv /usr/bin/sofetch.py /usr/bin/sofetch && sudo chmod +x /usr/bin/sofetch && rm -rf /tmp/sofetch/ && cd ~")
+     print("Done with setup.py")
+     sys.exit()
+
 print("Hello and Welcome to the installer for sofetch")
 print("Have you downloaded the repo locally [y/n]")
 input = input()
@@ -30,8 +36,8 @@ if input == "y":
     os.system("pip install --use-pep517 -r ", inputrepo, "requirements.txt && sudo mv ", inputrepo, "sofetch/* /usr/bin/ && sudo mv /usr/bin/sofetch.py /usr/bin/sofetch && sudo chmod +x /usr/bin/sofetch")
     print("done..")
 elif input == "n":
-     print("Ok dont worry we will do some things to get it ready")
-     if "Manjaro" in dist.name():
+    print("Ok dont worry we will do some things to get it ready")
+    if "Manjaro" in dist.name():
         os.system("sudo pacman -S git python3 python-pip")
     elif "Fedora" in dist.name():
         os.system("sudo dnf install git python3 python3-pip")
